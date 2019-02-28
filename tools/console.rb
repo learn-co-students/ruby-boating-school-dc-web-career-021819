8b3b8c6fe499190be4eaaedb636f6ff3005341ab
seanlucius <seanlucius22@icloud.com>
2019-02-27 22:49:53 -0500

    boatsnhoes

--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
 
+spongebob = Student.new("Spongebob")
+patrick= Student.new("Patrick")
+
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+
+boating_test1 = BoatingTest.new(spongebob, "Don't Crash 101", "pending", puff)
+boating_test2 = BoatingTest.new(patrick, "Power Steering 202", "failed", puff)
+boating_test3 = BoatingTest.new(patrick, "Power Steering 201", "passed", krabs)
+boating_test4 = BoatingTest.new(patrick, "Leedle", "passed", krabs)
+
+
+binding.pry 
+0
